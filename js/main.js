df3404c4fc5ab19e541eb3722dbc1d484c144891
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-17 23:13:07 +0200

    Select ID/CLASS

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
 
     // console.log(document.querySelectorAll('p'));
 
-    $('form, ul, p').css('fontSize', '30px');
+    // SELCT ELEMENT
+    // $('form, ul, p').css('fontSize', '30px');
+
+
+    // SELECT ID/CLASS
+    $("#list").css('fontSize', '30px');
+
+    $(".text").css('fontSize', '30px');
 
 });
