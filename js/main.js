fe497c66ce72411ede546f6a435354e491e1ad5e
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-18 21:17:05 +0200

    SELECT CHILDREN

--- js/main.js
+++ js/main.js
@@ -2,13 +2,22 @@ $(document).ready(function() {
 
     // console.log(document.querySelectorAll('p'));
 
+
+
+
     // SELCT ELEMENT
     // $('form, ul, p').css('fontSize', '30px');
+
+
+
 
 
     // SELECT ID/CLASS
     // $("#list").css('fontSize', '30px');
     // $(".text").css('fontSize', '30px');
+
+
+
 
 
     // SELECT OTHER ELEMENT
@@ -17,12 +26,24 @@ $(document).ready(function() {
     // $("input[type='radio']").css("margin", '30px');
     // $("input:checked").css("margin", '30px');
 
-    $("li:first").css("background", "red")
-    $("li:last").css("background", "red")
-    $("li:odd").css("background", "yellow")
+    // $("li:first").css("background", "red")
+    // $("li:last").css("background", "red")
+    // $("li:odd").css("background", "yellow")
 
-    $("tr:odd").css("background", "red");
+    // $("tr:odd").css("background", "red");
 
+
+
+
+
+
+    // SELECT CHILDREN
+    // $(".parent p").css("background", "red");
+    // $(".parent p:first-child").css("background", "red");
+    // $(".parent p:first-of-type").css("background", "red");
+
+    // $(".parent p:nth-child(2)").css("background", "red");
+    $(".parent p:nth-last-child(1)").css("background", "red");
 
 
 
